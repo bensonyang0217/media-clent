ee048590356d1cb7b1ad6fecc2b7c2d3e44edae8
Benson Yang <f932037@gmail.com>
2024-02-22 13:36:21 +0800

    update

--- src/api/media/media.js
+++ src/api/media/media.js
@@ -1,29 +1,67 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../../compoment/NavBar";
 import "../../css/MediaPage.css";
+
 const MediaForm = () => {
   const [file, setFile] = useState(null);
   const [uploads, setUploads] = useState([]);
+  const [uploadMessage, setUploadMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // 添加这个状态
+
+  const imagesUrl = "https://media-api-v2-f4hfj5ldpa-de.a.run.app/api/images";
+  useEffect(() => {
+    const fetchThumbnails = async () => {
+      try {
+        const response = await axios.get(imagesUrl, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        console.log(response.data);
+        setUploads(response.data); // 假设响应数据是一个包含所有上传信息的数组
+      } catch (error) {
+        console.error("Error fetching thumbnails", error);
+      }
+    };
+
+    fetchThumbnails();
+  }, []);
+
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
+    setUploadMessage("");
   };
 
   const handleUpload = async () => {
     if (!file) return;
+    setIsLoading(true); // 开始上传时设置为true
     const formData = new FormData();
     formData.append("file", file);
-
+    const uploadFileUrl =
+      "https://media-api-v2-f4hfj5ldpa-de.a.run.app/api/images/upload";
     try {
-      const response = await axios.post("/api/upload", formData, {
+      await axios.post(uploadFileUrl, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
-      setUploads([...uploads, response.data]); // 假设响应数据包含上传信息
+      const thumbnailsResponse = await axios.get(imagesUrl, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setUploadMessage("upload！");
+      setFile(null); // 清除已上传的文件
+      document.querySelector(".file-input").value = "";
+      //   console.log(thumbnailsResponse.data);
+      setUploads(thumbnailsResponse.data);
     } catch (error) {
-      console.error("上传失败", error);
+      console.error("upload error", error);
+      setUploadMessage("upload error");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -32,14 +70,23 @@ const MediaForm = () => {
       <NavBar />
       <div className="media-form-container">
         <h1>檔案上傳</h1>
-        <br />
+        {isLoading && (
+          <div className="loading-overlay">
+            <div className="loading-message">Loading...</div>
+          </div>
+        )}
+        {uploadMessage && <div className="upload-message">{uploadMessage}</div>}
         <div className="upload-section">
           <input
             type="file"
             onChange={handleFileChange}
             className="file-input"
           />
-          <button onClick={handleUpload} className="upload-button">
+          <button
+            onClick={handleUpload}
+            className="upload-button"
+            disabled={isLoading}
+          >
             Upload
           </button>
         </div>
@@ -48,25 +95,35 @@ const MediaForm = () => {
           <table className="uploads-table">
             <thead>
               <tr>
+                <th>Id</th>
                 <th>File Name</th>
-                <th>Size</th>
+                <th>Origin Size</th>
+                <th>Scaling Size</th>
                 <th>Scaling Status</th>
-                <th>Url</th>
+                <th>Upload Time</th>
+                <th>Image</th>
+                <th>Download</th>
               </tr>
             </thead>
             <tbody>
               {uploads.map((upload, index) => (
                 <tr key={index}>
+                  <td>{index + 1}</td>
                   <td>{upload.fileName}</td>
-                  <td>{upload.size}</td>
-                  <td>{upload.scalingStatus}</td>
+                  <td>{upload.fileSize}</td>
+                  <td>{upload.scalingSize ? upload.scalingSize : 0}</td>
+                  <td>{upload.thumbnailStatus ? "已完成" : "執行中"}</td>
+                  <td>{upload.uploadDate}</td>
+                  <td>
+                    <img src={upload.thumbnailUrl} alt={upload.fileName} />
+                  </td>
                   <td>
                     <a
-                      href={upload.url}
+                      href={upload.thumbnailUrl}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      View
+                      Download
                     </a>
                   </td>
                 </tr>
